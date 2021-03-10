e03359029cefa2bbe0f96e9e4b9645d1087a5d5d
Valeriia-Ro <vkolchak17@gmail.com>
2021-03-10 17:52:06 +0200

    oh

--- js/task-06.js
+++ js/task-06.js
@@ -6,20 +6,26 @@ inputEl.addEventListener('blur', checkedInput);
 
 function checkedInput(event) {
     
-    
+    function updateClass(addClass, remClass) {
+        inputEl.classList.add = addClass;
+        inputEl.classList.remove = remClass;
+    }
 
     if (event.currentTarget.value.length === numb) {
-        event.target.classList.add('valid').remove('invalid');
+        return updateClass(valid, invalid);
         // event.target.classList.remove('invalid');
         // event.target.classList.add('valid');
 
-    return true;
+    // return true;
     } 
-        event.target.classList.add('invalid').remove('valid');
+    return updateClass(('invalid'), ('valid'));
         // event.target.classList.remove('valid');
         // event.target.classList.add('invalid');
 
-    return false;
+    // return false;
 };
 
+
+
+console.log(checkedInput);
 
