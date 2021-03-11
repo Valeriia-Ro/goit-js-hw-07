02bdb23fa10df92c17b44dd15ef91b0cc116249d
Valeriia-Ro <vkolchak17@gmail.com>
2021-03-11 17:25:30 +0200

    it doesn't work

--- js/task-06.js
+++ js/task-06.js
@@ -5,26 +5,33 @@ const numb = Number(inputEl.getAttribute('data-length'));
 inputEl.addEventListener('blur', checkedInput);
 
 function checkedInput(event) {
-    
-    
+
     if (event.currentTarget.value.length === numb) {
-        return updateClass(('valid'), ('invalid'));
-        // event.target.classList.remove('invalid');
-        // event.target.classList.add('valid');
+        updateClass('valid', 'invalid');
+        } else {
+        updateClass('invalid', 'valid');
+    }
+
+
+    // if (event.currentTarget.value.length === numb) {
+       
+    //     event.target.classList.remove('invalid');
+    //     event.target.classList.add('valid');
 
     // return true;
-    } 
-    return updateClass(('invalid'), ('valid'));
-        // event.target.classList.remove('valid');
-        // event.target.classList.add('invalid');
+    // } 
+
+    //     event.target.classList.remove('valid');
+    //     event.target.classList.add('invalid');
 
     // return false;
+
 };
 
 function updateClass(addClass, remClass) {
     inputEl.classList.add = addClass;
     inputEl.classList.remove = remClass;
-}
+    }
 
 
 console.log(checkedInput);
