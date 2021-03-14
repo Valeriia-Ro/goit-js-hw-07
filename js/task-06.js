8e57cad0b6e93c95c5060e770d13a57a3bd8e85e
Valeriia-Ro <vkolchak17@gmail.com>
2021-03-14 14:36:28 +0200

    done

--- js/task-06.js
+++ js/task-06.js
@@ -4,35 +4,25 @@ const numb = Number(inputEl.getAttribute('data-length'));
 
 inputEl.addEventListener('blur', checkedInput);
 
+
 function checkedInput(event) {
+if (event.currentTarget.value.length === numb) {
+updateClass("valid", "invalid");
+} else {
+updateClass("invalid", "valid");
+}
+}
 
-
-    if (event.currentTarget.value.length === numb) {
-       
-        // event.target.classList.remove('invalid');
-        // event.target.classList.add('valid');
-        updateClass('valid', 'invalid');
-    // return true;
-    } 
-        updateClass('invalid', 'valid');
-        // event.target.classList.remove('valid');
-        // event.target.classList.add('invalid');
-
-    // return false;
-
-};
+function updateClass(addClass, remClass) {
+inputEl.classList.remove(remClass);
+inputEl.classList.add(addClass);
+}
 
 
 inputEl.addEventListener('focus', () => {
     inputEl.classList.remove('valid', 'invalid');
 });
 
-function updateClass(addClass, remClass) {
-    inputEl.classList.remove(remClass);
-    inputEl.classList.add(addClass);
-    
-}
 
 
-console.log(checkedInput);
 
